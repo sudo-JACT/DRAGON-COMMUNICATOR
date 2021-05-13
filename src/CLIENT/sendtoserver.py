e1072549f0506016bcf9e8732ff43672c57a3fc0
1mdonk3y <67701333+sudo-JACT@users.noreply.github.com>
2021-05-13 18:47:53 +0200

    Input update
    Now when you whant to write something there is CLIENT:

--- src/CLIENT/sendtoserver.py
+++ src/CLIENT/sendtoserver.py
@@ -1,7 +1,10 @@
+from rich.console import Console
+console = Console()
+
 def send_to_server(clsock,FLAG):
    
     while True:
         if FLAG==True:
             break
-        send_msg=input("")
+        send_msg=console.input(f"[bold gray]CLIENT: [/bold gray]")
         clsock.sendall(send_msg.encode())  #-->encode the message and sent it to server
