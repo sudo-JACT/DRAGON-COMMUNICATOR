5f157f05392534d18a193bc93997190cc09f8d17
1mdonk3y <67701333+sudo-JACT@users.noreply.github.com>
2021-05-18 17:36:08 +0200

    Bugs fix
    the graphic bug has been fixed

--- src/CLIENT/recivefromserver.py
+++ src/CLIENT/recivefromserver.py
@@ -10,12 +10,12 @@ def recv_from_server(clsock,FLAG):
         data=clsock.recv(1024).decode()
         
         if data=="q":
-            console.print(f"[bold red][WARNING!] CLOSING CONNECTION[/bold red]")
+            console.print(f"\n[bold red][WARNING!] CLOSING CONNECTION[/bold red]")
             console.print(f"[bold red][WARNING!] CONNECTION CLOSED[/bold red]")
             console.print(f"[bold red][WARNING!] QUITTED[/bold red]")
             FLAG==True
             break
         
-        console.print(f"[bold blue][SERVER] [/bold blue]{data}")  #--> print the message
+        console.print(f"\n[bold blue][SERVER] [/bold blue]{data}")  #--> print the message
         
         #playsound("91011.wav")--> if you want to play sound when the message is recived
